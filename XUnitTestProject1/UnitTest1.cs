fce5d9603741bd086bbb85fbdd7633a571707145
georgesaid@gmail.com <georgesaid@gmail.com>
2019-03-27 14:28:06 +0200

    EnsureRelativeTo() and EnsureAbsolutePath() are added to PathComposer

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -2,25 +2,76 @@ using System;
 using System.IO;
 using Xunit;
 using YouSubtle;
+using YouSubtle.IO;
 
 namespace XUnitTestProject1
 {
 	public class UnitTest1
 	{
-		[Fact]
-		public void Test1()
-		{
-			try
-			{
-				var foundFiles =  new DirectoryInfo(@"D:\CsScriptExecuter\ConsoleApp1\bin\Debug\netcoreapp2.1")
-				.GetDescendentFiles("System.Runtime.dll");
-			}
-			catch(Exception)
-			{
-				Assert.True(false);
-			}
+		//[Fact]
+		//public void Test1()
+		//{
+		//	try
+		//	{
+		//		var foundFiles =  new DirectoryInfo(@"D:\CsScriptExecuter\ConsoleApp1\bin\Debug\netcoreapp2.1")
+		//		.GetDescendentFiles("System.Runtime.dll");
+		//	}
+		//	catch(Exception)
+		//	{
+		//		Assert.True(false);
+		//	}
 			
 				
+		//}
+		[Fact]
+		public void PathComposer_EnsureRelativeTo_NonrelatedPathAndReference()
+		{
+			string path = @"D:\Test1\Test2";
+			string referenceDir = @"C:\Windows";
+
+			void faulty()
+			{
+				PathComposer.EnsureRelativeTo(referenceDir, path);
+			};
+
+			Assert.Throws<InvalidOperationException>(()=>faulty());
 		}
+
+		[Fact]
+		public void PathComposer_EnsureAbsolute_NonrelatedPathAndReference()
+		{
+			string path = @"D:\Test1\Test2";
+			string referenceDir = @"C:\Windows";
+
+			void faulty()
+			{
+				PathComposer.EnsureAbsolutePath(referenceDir, path);
+			};
+
+			Assert.Throws<InvalidOperationException>(() => faulty());
+		}
+
+		[Fact]
+		public void PathComposer_EnsureAbsolute_ValidAbsPathAndReference()
+		{
+			string path = @"D:\Test1\Test2";
+			string referenceDir = @"D:\";
+
+			string result = PathComposer.EnsureAbsolutePath(referenceDir, path);
+
+			Assert.True(result == path);
+		}
+
+		[Fact]
+		public void PathComposer_EnsureRelativeTo_ValidAbsPathAndReference()
+		{
+			string path = @"D:\Test1\Test2";
+			string referenceDir = @"D:\";
+
+			string result = PathComposer.EnsureRelativeTo(referenceDir, path);
+
+			Assert.True(result == "Test1\\Test2");
+		}
+
 	}
 }
--- YouSubtle.IO/PathComposer.cs
+++ YouSubtle.IO/PathComposer.cs
@@ -20,5 +20,63 @@ namespace YouSubtle.IO
 			segments = segments.Select(s => s.TrimSpecificSubstring(pathSeparator)).ToArray();
 			return string.Join(pathSeparator, segments);
 		}
+
+		/// <summary>
+		/// 1. In case path is an absolute one and belongs to the reference directory it returns the relative path (with no preceding path separator character).
+		/// 2. In case path is an absolute one but doesn't belong to the reference directory it throws an InvalidOperationException.
+		/// 3. In case path is already a relative path it returns it and removes the starting path separator character if any.
+		/// </summary>
+		/// <param name="referenceDir"></param>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public static string EnsureRelativeTo(string referenceDir, string path)
+		{
+			if(path.Contains(":")) // absolute
+			{
+				if (path.StartsWith(referenceDir) == false)
+				{
+					throw new InvalidOperationException($"path [{path}] cannot be relative to [{referenceDir}]");
+				}
+				else
+				{
+					// ommit the reference dir.
+					string relativePath = path.Substring(referenceDir.Length);
+					if(relativePath.StartsWith("/") || relativePath.StartsWith("\\"))
+					{
+						relativePath = relativePath.Substring(1);
+					}
+					return relativePath;
+				}
+			}
+			else // relative
+			{
+				return path;
+			}
+		}
+
+		/// <summary>
+		/// 1. In case path is an absolute one and belongs to the reference directory it returns it as is.
+		/// 2. In case path is an absolute one but doesn't belong to the reference directory it throws an InvalidOperationException.
+		/// 3. In case path is a relative path it returns the absolute path based on the reference directory.
+		/// </summary>
+		/// <param name="referenceDir"></param>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public static string EnsureAbsolutePath(string referenceDir, string path)
+		{
+			if (path.Contains(":")) // absolute
+			{
+				if (path.StartsWith(referenceDir) == false)
+				{
+					throw new InvalidOperationException($"path [{path}] cannot be relative to [{referenceDir}]");
+				}
+
+				return path;
+			}
+			else // relative
+			{
+				return Combine(referenceDir, path);
+			}
+		}
 	}
 }
